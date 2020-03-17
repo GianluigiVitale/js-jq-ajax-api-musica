edfdb6c43717e72cba6cb0fb33259549079487db
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-03-17 16:20:43 +0100

    handlebar template

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+/*
+Attraverso una chiamata ajax all’Api di boolean avremo a disposizione una decina di dischi musicali.
+Servendoci di handlebars stampiamo tutto a schermo. In questo momento non è importante la parte grafica.
+Bonus: Creare una select con i seguenti generi: pop, rock, metal e jazz.
+In base a cosa scegliamo nella select vedremo i corrispondenti cd.
+*/
+
+
 $(document).ready(function() {
-	//Code
+
+    var source = $("entry-template").html();
+    var template = Handlebars.compile(source);
+
+    $.ajax({
+
+    });
+
+
 });
